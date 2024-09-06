dd5336a37387439cded776a8c0f61a75e34eaf6e
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2024-09-06 15:51:53 +0000

    Included the html-webpack-plugin settings and devServer settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
 
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 export default {
     entry: './src/scripts/main.js',
     output: {
         filename: 'bundle.js',
         path: resolve('./dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        contentBase: resolve('./src'),
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
     },
     module: {
         rules: [
@@ -21,6 +30,12 @@ export default {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: 'index.html',
+        }),
+    ],
     mode: 'development',
     devtool: 'inline-source-map',
 };
